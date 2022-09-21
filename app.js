cc0e1d5f00142b9f7a21880ad3f325d7fdb3cfa1
TRANDUCTHANGB1910450 <thangb1910450@student.ctu.edu.vn>
2022-09-21 09:19:12 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -2,13 +2,23 @@ const express = require("express");
 const cors = require("cors");
 const contactRouter = require("./app/routes/contact.route");
 const app =express();
+const ApiError = require("./app/api-error");
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactRouter);
-
-app.get("/", (req, res)=>{
-    res.json({ message: "Welcomto contact book application." });
+app.use((req,res,next) => {
+    return next(new ApiError(404, "Resoure not found"));
 });
+
+app.use((err,req,res,next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
+
+// app.get("/", (req, res)=>{
+//     res.json({ message: "Welcomto contact book application." });
+// });
 
 module.exports = app;
